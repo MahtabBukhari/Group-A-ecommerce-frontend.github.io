8af68e31e8b1272e41b2d51d894f0d62136fca6f
MahtabBukhari <git config --global user.email you@example.com>
2022-09-16 09:32:27 +0500

    implimant overlay-navebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
-import React from 'react'
+import React from 'react';
+import {BrowserRouter as Router} from "react-router-dom";
+import Header from './component/layout/header'
 
 function App() {
   return (
-    <div>
-      app
-    </div>
+    <Router>
+      <Header/>
+    </Router>
   )
 }
 
---
+++ src/component/layout/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {ReactNavbar} from "overlay-navbar"
+
+const header = () => {
+  return <ReactNavbar/>
+    
+   
+}
+
+export default header
